1904ff5ecbcad1200749125b1560ca2f340ac7c6
Denis Zavershinskiy <zaverden@gmail.com>
2021-07-03 14:46:19 +0700

    [PROJ-42] add comments to the script

--- .lefthook/commit-msg/commit-prefix.js
+++ .lefthook/commit-msg/commit-prefix.js
@@ -1,10 +1,13 @@
 const { promises: fsp } = require("fs");
 
+// https://git-scm.com/docs/githooks#_commit_msg
+// commit-msg takes a single parameter, the name of the file that holds the proposed commit log message
 const [, , messageFilePath] = process.argv;
 
 const regex = /^\[PROJ\-\d+\]/;
 
 /**
+ * Checks if we like the message
  * @param {string} message
  * @returns { boolean }
  */
@@ -12,12 +15,21 @@ function testMessage(message) {
   return regex.test(message);
 }
 
-fsp.readFile(messageFilePath, { encoding: "utf8" }).then((commitMessage) => {
+(async () => {
+  // Read the commit message from the file
+  const commitMessage = await fsp.readFile(messageFilePath, {
+    encoding: "utf8",
+  });
+
+  // Check if it is prefixed
   const valid = testMessage(commitMessage);
 
   if (!valid) {
-    return fsp.writeFile(messageFilePath, `[PROJ-42] ${commitMessage}`, {
-      encoding: "utf8",
-    });
+    // If the message is not prefixed add hardcoded prefix
+    // TODO: extract prefix from the branch name
+    const prefixedMessage = `[PROJ-42] ${commitMessage}`;
+
+    // write the prefixed message to the commit message file
+    await fsp.writeFile(messageFilePath, prefixedMessage, { encoding: "utf8" });
   }
-});
+})();
